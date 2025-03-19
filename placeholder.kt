e27027a814433a652186ce46a16646034298528f
Yousef AlShaikh <164582835+yousefalshaikh17@users.noreply.github.com>
2025-03-19 19:38:41 +0000

    Adjusted kotlin placeholder

--- placeholder.kt
+++ placeholder.kt
@@ -1,4 +1,20 @@
 // Looking at open sourcing this project in the future!
+
+import java.util.Date
+
+// A simple function to greet a user
+fun greetUser(name: String) {
+    println("Hello, $name!")
+}
+
+// Main function to start the program (There is no real program at the moment)
 fun main() {
     println("Hello, world!")
+    
+    // Call greetUser with an example name
+    greetUser("Kotlin Developer")
+    
+    // Print some additional information
+    val today = Date()
+    println("Today's date is: $today")
 }
